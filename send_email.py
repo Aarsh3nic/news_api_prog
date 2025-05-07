e169b3c7d7f2449a4448ffa365289ccd0ecd074b
Aarsh Patel <50718440+Aarsh3nic@users.noreply.github.com>
2025-05-07 14:33:40 -0400

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,10 +5,10 @@ def send_email_news(news):
     host ="smtp.gmail.com"
     port = 465
 
-    username = "YourEmail"
+    username = "SendingEmail"
     password = "passwordOfYourAcc"  # os.getenv("PASSforsecondarygmail") can be used to get password from env variable
 
-    receiver = "5mindisc@gmail.com"
+    receiver = "ReceivingEmail"
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context = context) as server:
